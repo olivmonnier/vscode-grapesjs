4f2ce6b808a5b80dd6c13e80dd3118dcdf59dd14
olivmonnier <omonnier.dev@gmail.com>
2019-06-29 18:44:31 +0200

    clean code

--- src/extension/utils.ts
+++ src/extension/utils.ts
@@ -1,16 +1,3 @@
-export function debounced(delay: number, fn: Function) {
-  let timerId: any;
-  return function(...args: any[]) {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-    timerId = setTimeout(() => {
-      fn(...args);
-      timerId = null;
-    }, delay);
-  };
-}
-
 export function getNestedObject(nestedObj: any, pathArr: Array<string>) {
   return pathArr.reduce(
     (obj, key) => (obj && obj[key] !== 'undefined' ? obj[key] : undefined),
